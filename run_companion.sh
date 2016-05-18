222aa89431ef8989fe7aa49365e7080a3c5dca41
Martijn Kamstra <mkamstra@gmail.com>
2016-05-18 14:52:08 +0100

    export SPARK-JAVA_OPTS added to be able to use remote debugger

--- run_companion.sh
+++ run_companion.sh
@@ -1,4 +1,6 @@
 echo 'Running CompanionRiskFactors with' $# 'arguments:' $1 $2 $3
 
+export SPARK_JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
+
 /usr/local/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --driver-memory 2g --class 'no.stcorp.com.companion.CompanionRiskFactors' \
 --master local[*] target/CompanionWeatherTraffic-0.1-jar-with-dependencies.jar -$1 $2,$3
