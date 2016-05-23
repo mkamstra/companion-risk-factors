bd7a724f16c199539b56a01b86940539ac1470ea
Martijn Kamstra <mkamstra@gmail.com>
2016-05-23 15:28:56 +0100

    Consistent spelling of series names

--- src/test/java/no/stcorp/com/companion/visualization/TimeSeriesPlotterTest.java
+++ src/test/java/no/stcorp/com/companion/visualization/TimeSeriesPlotterTest.java
@@ -45,10 +45,10 @@ public class TimeSeriesPlotterTest {
         }
       }
     }
-    assertEquals(720, tdc.getTrafficspeedSeries().getItemCount());
+    assertEquals(720, tdc.getTrafficSpeedSeries().getItemCount());
     assertEquals(240, tdc.getTemperatureSeries().getItemCount());
     assertEquals(240, tdc.getPrecipitationSeries().getItemCount());
-    assertEquals(240, tdc.getWindspeedSeries().getItemCount());
+    assertEquals(240, tdc.getWindSpeedSeries().getItemCount());
     tdc.writeDataToFile("./", ndwId, timeStartString, timeEndString);
     TimeSeriesPlotter tsp = new TimeSeriesPlotter("Weather and traffic at measurement site " + ndwId);
     tsp.plot(ndwId, timeStartString, timeEndString, tdc);
@@ -71,10 +71,10 @@ public class TimeSeriesPlotterTest {
     tdc.importDataSeries(TimeSeriesDataContainer.SeriesType.TEMPERATURE, fileNameTemperature);
     tdc.importDataSeries(TimeSeriesDataContainer.SeriesType.PRECIPITATION, fileNamePrecipitation);
     tdc.importDataSeries(TimeSeriesDataContainer.SeriesType.WINDSPEED, fileNameWindspeed);
-    assertEquals(137, tdc.getTrafficspeedSeries().getItemCount());
+    assertEquals(137, tdc.getTrafficSpeedSeries().getItemCount());
     assertEquals(24, tdc.getTemperatureSeries().getItemCount());
     assertEquals(24, tdc.getPrecipitationSeries().getItemCount());
-    assertEquals(24, tdc.getWindspeedSeries().getItemCount());
+    assertEquals(24, tdc.getWindSpeedSeries().getItemCount());
     TimeSeriesPlotter tsp = new TimeSeriesPlotter("Weather and traffic at measurement site " + ndwId);
     tsp.plot(ndwId, timeStartString, timeEndString, tdc);
   }
