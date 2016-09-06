76d35121ac151795f550d856be194092cf51350d
Erwin Torreao Dassen <erwin.dassen@stcorp.nl>
2016-09-06 16:20:38 +0200

    Bug fixes on the docker container;

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
-docker run --name companion_rf --link companion_db:companion_db -v /Volumes/CompanionEx/Debug/hdf:/root/hdf -v /Volumes/CompanionEx/Data/ndw/:/root/ndw -v /Volumes/CompanionEx/Debug/kml/:/root/kml companion_rf:latest > /dev/null &
+docker run --name companion_rf --link companion_db:companion_db -v /tmp:/tmp -v /Volumes/CompanionEx/Debug/hdf:/root/hdf -v /Volumes/CompanionEx/Data/ndw/:/root/ndw -v /Volumes/CompanionEx/Debug/kml/:/root/kml companion_rf:latest > /dev/null &
 
